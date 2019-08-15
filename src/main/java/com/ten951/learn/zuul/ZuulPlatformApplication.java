418e4f8b7f6f0ca421565ed6a8b0223f7cacd374
wangyongtian <ten951@qq.com>
2019-08-15 14:43:58 +0800

    first commit

---
+++ src/main/java/com/ten951/learn/zuul/ZuulPlatformApplication.java
@@ -0,0 +1,16 @@
+package com.ten951.learn.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yongtianwang
+ */
+@SpringBootApplication
+public class ZuulPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulPlatformApplication.class, args);
+    }
+
+}
